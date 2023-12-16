6169f153875f35b47391760951ed1b428c832c47
Erick Torres <retromax2025@gmail.com>
2023-12-15 16:57:05 -0800

    latest Update to Project

--- scripts/index.js
+++ scripts/index.js
@@ -39,6 +39,9 @@ const profileDescriptionInput = document.getElementById(
   "profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                   Functions;                                   ||
@@ -46,6 +49,17 @@ const profileEditForm = profileEditModal.querySelector(".modal__form");
 
 function closePopup() {
   profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+
+  return cardElement;
 }
 
 // ! ||--------------------------------------------------------------------------------||
@@ -74,3 +88,8 @@ profileEditButton.addEventListener("click", () => {
 });
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
